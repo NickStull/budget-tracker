7fe3b58ba775d1a065153b40ea5a561ce87cddc7
Nick Stull <73801944+NickStull@users.noreply.github.com>
2021-02-28 15:52:48 -0600

    added webpack

---
+++ public/webpack.config.js
@@ -0,0 +1,42 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  mode: "development",
+
+  // add entry points for JavaScript files for the three pages, home, detail, and favorites.
+  entry: {
+    index: "./index.js"
+  },
+  output: {
+    path: __dirname + "/dist",
+    filename: "[name].bundle.js"
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: false,
+      name: "Budget Tracker App",
+      short_name: "Budget App",
+      description: "An application to keep track of a Budget that will work in offline situations",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      start_url: "/",
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  }
+};
+module.exports = config;
